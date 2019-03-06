0721bc463becd037f70c74046748751b2e396f1e
Dan <danandreist@gmail.com>
2019-03-06 10:29:33 +0200

    Updates

---
+++ precache-manifest.3065dc5a46d975ddc11c70328636c452.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93cf5dff009ee0bfd32c",
+    "url": "/sqoodleadmin/static/js/runtime~main.93cf5dff.js"
+  },
+  {
+    "revision": "1034121d0a347deb8232",
+    "url": "/sqoodleadmin/static/js/main.1034121d.chunk.js"
+  },
+  {
+    "revision": "eada0a95502d0aae58fc",
+    "url": "/sqoodleadmin/static/js/2.eada0a95.chunk.js"
+  },
+  {
+    "revision": "1034121d0a347deb8232",
+    "url": "/sqoodleadmin/static/css/main.eaf1ce7e.chunk.css"
+  },
+  {
+    "revision": "bd18efa80d7c8459c8616ee5d13980f1",
+    "url": "/sqoodleadmin/index.html"
+  }
+];
